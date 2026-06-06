42803e3a375bb5ce836611e578459db3b84dc290
Commonly <51011212+commonly-ts@users.noreply.github.com>
2026-06-06 19:11:49 +1200

    feat: added chunkArray

--- src/functions/query.ts
+++ src/functions/query.ts
@@ -90,3 +90,11 @@ export function getOptionName(
 	const options = command.data.options;
 	return options.find((o) => o.name[searchBy](partial))?.name;
 }
+
+export function chunkArray<T>(array: T[], size: number): T[][] {
+	const result: T[][] = [];
+	for (let i = 0; i < array.length; i += size) {
+		result.push(array.slice(i, i + size));
+	}
+	return result;
+}
